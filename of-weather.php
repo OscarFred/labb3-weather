ed25fa1a9f83556958421b0b3ebc7911a18ed22e
OscarFred <mossisoscar@gmail.com>
2020-11-24 20:37:50 +0100

    La till en funktion för att hämta väder och en funktion som kollar så det bara visas på vald sida

--- of-weather.php
+++ of-weather.php
@@ -10,46 +10,68 @@ if ( ! defined('ABSPATH')) {
     die;
 }
 
-class ofWeather {
-    function __construct() {
-        add_action('init', array($this, 'custom_post_type'));
-    }
-    function activate() {
-        
-    }
-
-    function deactivate() {
-
-    }
-    
-    function uninstall() {
-
-    }
-    
-    function custom_post_type() {
-        register_post_type('product', ['public' => true, 'label' => 'Product']);
-    }
-}
 if ( class_exists('ofWeather')) {
-    $ofWeather = new ofWeather('TESTHEST');
+    $ofWeather = new ofWeather();
 }
-add_action('acf/init', 'my_acf_op_init');
-function my_acf_op_init() {
 
-    // Check function exists.
+class ofWeather {
+    public $selected_page;
+    public $result;
+    public $weather;
+
+
+    public function __construct() {
+        add_action('wp_head', array($this, 'which_page'));
+    }
+    public function which_page() {       
+        $this->selected_page = get_field('of_weather', 'options');
+        switch ($this->selected_page) {
+            case 'specific_product':
+                if ( is_product() && wc_get_product()->get_id() === get_field('specific_product', 'options') ) {
+                    add_action('woocommerce_before_single_product', array($this, 'weather'));
+                }
+                break;
+            case 'cart':
+                add_action('woocommerce_before_cart', array($this, 'weather'));
+                break;
+            case 'shop':
+                add_action('woocommerce_before_shop_loop', array($this, 'weather'));
+                break;
+            case 'checkout':
+                add_action('woocommerce_before_checkout_form', array($this, 'weather'));
+                break;
+            case 'product':
+                add_action('woocommerce_before_single_product', array($this, 'weather'));
+                break;
+        }
+    
+    }
+    public function weather () {
+        if (get_transient('Weather') === false) {
+            $this->result = wp_remote_get('http://api.openweathermap.org/data/2.5/weather?q=Gothenburg&units=metric&appid=455dd990cb94b9cccbd792dafe888831&lang=sv');
+            set_transient('Weather', $this->result, HOUR_IN_SECONDS);
+            }
+            $weather = json_decode(get_transient('Weather')['body'], true);
+            echo "Grader: {$weather['main']['temp']} C";
+            echo '<br>';
+            echo "Beskrivning: {$weather['weather'][0]['description']}";
+            echo '<br>';
+            echo "Vind hastighet: {$weather['wind']['speed']} m/s";
+            echo '<br>';
+            
+            
+        }
+    }
+
+add_action('acf/init', 'of_weather_init');
+function of_weather_init() {
     if( function_exists('acf_add_options_page') ) {
-
-        // Register options page.
         $option_page = acf_add_options_page(array(
             'page_title'    => __('OF Weather options'),
             'menu_title'    => __('OF Weather options'),
-            'menu_slug'     => 'theme-general-settings',
+            'menu_slug'     => 'of_weather_options',
             'capability'    => 'edit_posts',
             'redirect'      => false
         ));
     }
 }
-
-//activation
-register_activation_hook( __FILE__, array($ofWeather, 'activate') );
-register_deactivation_hook( __FILE__, array($ofWeather, 'deactivate') );
